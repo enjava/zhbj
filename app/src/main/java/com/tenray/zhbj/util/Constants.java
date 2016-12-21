f9ebc74e991d91f2e974525b756ae29c3926f6b7
enjava <enjava@163.com>
2016-12-21 09:53:34 +0800

    en

---
+++ app/src/main/java/com/tenray/zhbj/util/Constants.java
@@ -0,0 +1,28 @@
+package com.tenray.zhbj.util;
+
+public class Constants {
+	/**
+	 * 发送帧次数
+	 */
+	public static final String FRAME_NUMBER = "frame_number";
+	/**
+	 * 串口路径
+	 */
+	public static final String COM_PATH = "com_path";
+
+	/**
+	 * 货道
+	 */
+	public static final String CHANEL_NAME = "chanel_name";
+
+	/**
+	 * 货道
+	 */
+	public static final String MACHINE_ID = "machine_id";
+
+	/**
+	 * 广告视频列表
+	 */
+	public static final String AD_VIDEO_NAME ="ad_video_name" ;
+
+}
