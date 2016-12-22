993ec47b0b384f1fd9c163ada202c66f1a71479d
enjava <enjava@163.com>
2016-12-22 15:37:09 +0800

    en

--- app/src/main/java/com/tenray/zhbj/activity/MainActivity.java
+++ app/src/main/java/com/tenray/zhbj/activity/MainActivity.java
@@ -1,14 +1,20 @@
 package com.tenray.zhbj.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.tenray.zhbj.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.left_menu);
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);// 全屏触摸
+        slidingMenu.setBehindOffset(400);// 屏幕预留200像素宽度
     }
 }
